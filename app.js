df3799e0099ed4be470b6d476def4f13f8b442bf
Dostmurod <dostmurodzoirov777@gmail.com>
2022-12-20 10:11:06 +0500

    Masala Tayyor

---
+++ app.js
@@ -0,0 +1,29 @@
+// 1-Masala
+
+// const arr = [true , false , true , true] 
+
+// const check = arr.filter(e => e === true).length;
+// console.log(check)
+
+// ==================================================================
+
+// 2-Masala
+
+// const arr = [10, 20, 30, 40];
+// const sum = arr.reduce((a, b) => a + b , 0);
+// console.log(sum)
+
+// 3-Masala
+// const numberSplit = (num) => {
+//     if (num === undefined) {
+//       return "Argument bosh qoldirildi!!!"
+//     } else {
+//     let a = Math.floor(num / 2);
+//     let b = num - a;
+//     let c = [];
+//     c.push(a, b);
+//     return c;
+//     }
+//   };
+
+//  console.log( numberSplit(11))
